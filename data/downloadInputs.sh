0436a0c253e37eea4e4b8a15b0d94c6dd4eda18c
thadh <thadh@localhost>
2014-09-30 14:06:36 -0700

    .classpath, downloadInput.sh, todo

--- data/downloadInputs.sh
+++ data/downloadInputs.sh
@@ -8,14 +8,10 @@ echo "Note that unzipping is slow."
 
 L=en
 echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
-WIKI=${L}wiktionary-20131217-pages-meta-current.xml
-curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20131217/${WIKI}.bz2
+WIKI=${L}wiktionary-20140908-pages-articles.xml
+curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20140908/${WIKI}.bz2
 bunzip2 ${WIKI}.bz2
-mv ${WIKI} inputs/${L}wiktionary-pages-meta-current.xml
-
-return
-
-
+mv ${WIKI} inputs/${L}wiktionary-pages-articles.xml
 
 echo "Downloading from: http://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/"
 CHEMNITZ=de-en.txt
@@ -23,32 +19,31 @@ curl --remote-name http://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/${CH
 gunzip ${CHEMNITZ}.gz
 mv ${CHEMNITZ} inputs/de-en_chemnitz.txt
 
-
 L=fr
 echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
-WIKI=${L}wiktionary-20131222-pages-articles.xml
-curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20131222/${WIKI}.bz2
+WIKI=${L}wiktionary-20140910-pages-articles.xml
+curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20140910/${WIKI}.bz2
 bunzip2 --force ${WIKI}.bz2
 mv ${WIKI} inputs/${L}wiktionary-pages-articles.xml
 
 L=it
 echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
-WIKI=${L}wiktionary-20131215-pages-articles.xml
-curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20131215/${WIKI}.bz2
+WIKI=${L}wiktionary-20140923-pages-articles.xml
+curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20140923/${WIKI}.bz2
 bunzip2 ${WIKI}.bz2
 mv ${WIKI} inputs/${L}wiktionary-pages-articles.xml
 
 L=de
 echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
-WIKI=${L}wiktionary-20131220-pages-articles.xml
-curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20131220/${WIKI}.bz2
+WIKI=${L}wiktionary-20140905-pages-articles.xml
+curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20140905/${WIKI}.bz2
 bunzip2 ${WIKI}.bz2
 mv ${WIKI} inputs/${L}wiktionary-pages-articles.xml
 
 L=es
 echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
-WIKI=${L}wiktionary-20131228-pages-articles.xml
-curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20131228/${WIKI}.bz2
+WIKI=${L}wiktionary-20140920-pages-articles.xml
+curl --remote-name http://dumps.wikimedia.org/${L}wiktionary/20140920/${WIKI}.bz2
 bunzip2 ${WIKI}.bz2
 mv ${WIKI} inputs/${L}wiktionary-pages-articles.xml
 
