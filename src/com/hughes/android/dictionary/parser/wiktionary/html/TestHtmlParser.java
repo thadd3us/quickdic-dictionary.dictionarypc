3d9f611a75023bb08f84a8691aa936fa886f8c2a
thadh <thadh@localhost>
2015-06-02 07:22:44 -0700

    Test html parser.

---
+++ src/com/hughes/android/dictionary/parser/wiktionary/html/TestHtmlParser.java
@@ -0,0 +1,10 @@
+package com.hughes.android.dictionary.parser.wiktionary.html;
+
+public class TestHtmlParser {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
